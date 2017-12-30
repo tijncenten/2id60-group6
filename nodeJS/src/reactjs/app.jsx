cdac9707d7dbfb055846641cba17f5e6085a526b
Tijn Centen <tijncenten@hotmail.com>
2017-12-30 19:39:42 +0100

    Setup sass and react-md working

--- nodeJS/src/reactjs/app.jsx
+++ nodeJS/src/reactjs/app.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button, Toolbar } from 'react-md';
+import ShareVariantIcon from 'mdi-react/ShareVariantIcon';
+import style from '../scss/style.scss';
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>Het werkt!</div>
+      <div>
+        <Toolbar
+          colored
+          fixed
+          nav={<Button icon>menu</Button>}
+          title="Toolbar" />
+        <div className="md-toolbar-relative">
+        <p>Het werkt!</p>
+        <Button floating secondary>share</Button>
+        </div>
+      </div>
     );
   }
 }
--- nodeJS/webpack.common.js
+++ nodeJS/webpack.common.js
@@ -1,5 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin('../css/[name].bundle.css', {
+  filename: 'style.css'
+})
 
 module.exports = {
   entry: {
@@ -7,12 +12,14 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '..', 'socialMedia', 'static', 'js'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    publicPath: path.resolve(__dirname, '..', 'socialMedia', 'static', 'css')
   },
   module: {
     loaders: [
       {
         test: /\.jsx$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react'],
@@ -21,14 +28,25 @@ module.exports = {
       },
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015'],
           plugins: ['transform-regenerator']
         }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
       }
     ]
   },
+  plugins: [
+    extractPlugin
+  ],
   stats: {
     colors: true
   }
