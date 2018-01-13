02060c12c10165b7229c82f4887b49ed0416e04e
Julian Smits <juliansmits@hotmail.com>
2018-01-13 16:04:56 +0100

    start on styling login page

--- nodeJS/src/reactjs/Registration.jsx
+++ nodeJS/src/reactjs/Registration.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Router, Switch } from 'react-router-dom';
 import style from '../scss/style.scss';
+import { Button, Toolbar} from 'react-md';
 
 
 class Registration extends React.Component {
   render() {
     return (
-      <div dangerouslySetInnerHTML={{ __html: this.props.children }} />
+      <div>
+        <Toolbar
+          colored
+          title="Login"
+        />
+        <form method="post">
+          <div dangerouslySetInnerHTML={{ __html: this.props.children }} />
+
+          <Button flat primary type="submit">login</Button>
+        </form>
+      </div>
     );
   }
 }
