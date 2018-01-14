24d6cbf54268f177ce8fd182744a207a148e50e4
deguviro <deguviro@gmail.com>
2018-01-14 12:17:30 +0100

    update profile menu

--- nodeJS/src/reactjs/components/ProfileMenu.jsx
+++ nodeJS/src/reactjs/components/ProfileMenu.jsx
@@ -1,38 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Avatar, AccessibleFakeButton, IconSeparator, FontIcon, DropdownMenu } from 'react-md';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 
 const ProfileMenu = ({ simplifiedMenu }) => (
-  <DropdownMenu
-    id={`${!simplifiedMenu ? 'smart-' : ''}avatar-dropdown-menu`}
-    menuItems={[
-      <NavLink to={`/profile/${activeUser.username}`}>
-        Profile
-      </NavLink>,
-      { divider: true },
-      <a href="/logout">Log out</a>]}
-    anchor={{
-      x: DropdownMenu.HorizontalAnchors.INNER_RIGHT,
-      y: DropdownMenu.VerticalAnchors.BOTTOM,
-    }}
-    position={DropdownMenu.Positions.TOP_RIGHT}
-    animationPosition="below"
-    sameWidth
-    simplifiedMenu={simplifiedMenu}
-  >
-    <AccessibleFakeButton
-      component={IconSeparator}
-      iconBefore
-      label={
-        <IconSeparator label={activeUser.firstName}>
-          <FontIcon>arrow_drop_down</FontIcon>
-        </IconSeparator>
-      }
+  <Route render={({ history}) => (
+    <DropdownMenu
+      id={`${!simplifiedMenu ? 'smart-' : ''}avatar-dropdown-menu`}
+      menuItems={[
+        { primaryText: 'Profile', onClick: () => {
+          history.push('/profile/' + activeUser.username);
+        }},
+        { divider: true },
+        { primaryText: 'Log out', onClick: () => {
+          window.location.href = '/logout';
+        }}]}
+      anchor={{
+        x: DropdownMenu.HorizontalAnchors.INNER_RIGHT,
+        y: DropdownMenu.VerticalAnchors.BOTTOM,
+      }}
+      position={DropdownMenu.Positions.TOP_RIGHT}
+      animationPosition="below"
+      sameWidth
+      simplifiedMenu={simplifiedMenu}
     >
-      <Avatar suffix={activeUser.avatarColor}>{activeUser.firstName[0] + activeUser.lastName[0]}</Avatar>
-    </AccessibleFakeButton>
-  </DropdownMenu>
+      <AccessibleFakeButton
+        component={IconSeparator}
+        iconBefore
+        label={
+          <IconSeparator label={activeUser.firstName}>
+            <FontIcon>arrow_drop_down</FontIcon>
+          </IconSeparator>
+        }
+      >
+        <Avatar suffix={activeUser.avatarColor}>{activeUser.firstName[0] + activeUser.lastName[0]}</Avatar>
+      </AccessibleFakeButton>
+    </DropdownMenu>
+  )} />
 );
 
 ProfileMenu.propTypes = {
