d2cb50124bb44197d24b54f31bcf18e195a954fb
Julian Smits <juliansmits@hotmail.com>
2018-01-15 15:42:44 +0100

    added avatar profile pictures to chat

--- nodeJS/src/reactjs/components/chat/ChatList.jsx
+++ nodeJS/src/reactjs/components/chat/ChatList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Avatar, List, ListItem } from 'react-md';
+import profilePictureParser from '../../../js/utils/profilePictureParser';
 
 class ChatList extends React.Component {
   render() {
@@ -12,15 +13,27 @@ class ChatList extends React.Component {
     }
     return (
       <List className="chat-list">
-        {items.map(chat => (
+        {items.map(chat => {
+          let avatar;
+          if(chat.profile.profilePicture === null){
+            avatar = (
+              <Avatar suffix={chat.profile.avatarColor}>{chat.profile.firstName[0] + chat.profile.lastName[0]}</Avatar>
+            );
+          } else {
+            avatar = (
+              <Avatar src={profilePictureParser.parseThumb(chat.profile.profilePicture)}/>
+            );
+          }
+          return (
           <ListItem
             key={chat.profile.id}
-            leftAvatar={<Avatar random>{chat.profile.firstName[0] + chat.profile.lastName[0]}</Avatar>}
+            leftAvatar={avatar}
             primaryText={chat.profile.firstName + " " + chat.profile.lastName}
             secondaryText={chat.date}
             active={this.props.selectedId === chat.id}
             onClick={() => this.props.onItemClick(chat)} />
-        ))}
+          );
+        })}
       </List>
     );
   }
